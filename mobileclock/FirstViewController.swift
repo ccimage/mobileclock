ac4503d8e1ffa952d3d16b268f8b90e4b619c7de
chuan <zhangchuan.cn@gmail.com>
2018-05-12 18:57:20 +0800

    升级到新的swift语法

--- mobileclock/FirstViewController.swift
+++ mobileclock/FirstViewController.swift
@@ -24,14 +24,14 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        changeImageSize(false);
+        changeImageSize(fromPortrait: false);
         doTimer();
         
-        swipeRec1.direction = UISwipeGestureRecognizerDirection.Down;
-        swipeRec2.direction = UISwipeGestureRecognizerDirection.Up;
-        tapRec.addTarget(self, action: "tappedView");
-        swipeRec1.addTarget(self, action: "swipeViewDown:");
-        swipeRec2.addTarget(self, action: "swipeViewUp:");
+        swipeRec1.direction = UISwipeGestureRecognizerDirection.down;
+        swipeRec2.direction = UISwipeGestureRecognizerDirection.up;
+        tapRec.addTarget(self, action: #selector(FirstViewController.tappedView));
+        swipeRec1.addTarget(self, action: Selector(("swipeViewDown:")));
+        swipeRec2.addTarget(self, action: Selector(("swipeViewUp:")));
         mainView.addGestureRecognizer(tapRec);
         mainView.addGestureRecognizer(swipeRec1);
         mainView.addGestureRecognizer(swipeRec2);
@@ -42,20 +42,20 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated);
-        setTabBarVisible(!tabBarIsVisible(), animated: true)
+        setTabBarVisible(visible: !tabBarIsVisible(), animated: true)
 
         pointImageView.image = UIImage(named: String(format: "%@_number_11.png", theme));
     }
 
-    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
-        let fromPortrait:Bool = (fromInterfaceOrientation == UIInterfaceOrientation.Portrait) || (fromInterfaceOrientation == UIInterfaceOrientation.PortraitUpsideDown);
-        changeImageSize(fromPortrait);
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        let fromPortrait:Bool = (fromInterfaceOrientation == UIInterfaceOrientation.portrait) || (fromInterfaceOrientation == UIInterfaceOrientation.portraitUpsideDown);
+        changeImageSize(fromPortrait: fromPortrait);
     }
     
     func changeImageSize(fromPortrait:Bool){
-        let screenBounds:CGRect = UIScreen.mainScreen().bounds;
+        let screenBounds:CGRect = UIScreen.main.bounds;
         let width = screenBounds.size.width;
         var height = screenBounds.size.height;
         var eachWidth = width / 5;
@@ -64,24 +64,24 @@ class FirstViewController: UIViewController {
             height = width;
         }
 
-        hourImageView1.frame = CGRectMake(0, 0, eachWidth, height);
-        hourImageView2.frame = CGRectMake(eachWidth, 0, eachWidth, height);
-        pointImageView.frame = CGRectMake(eachWidth * 2, 0, eachWidth, height);
-        minuteImageView1.frame = CGRectMake(eachWidth * 3, 0, eachWidth, height);
-        minuteImageView2.frame = CGRectMake(eachWidth * 4, 0, eachWidth, height);
+        hourImageView1.frame = CGRect(origin:CGPoint(x:0, y:0), size:CGSize(width:eachWidth, height:height));
+        hourImageView2.frame = CGRect(origin:CGPoint(x:eachWidth, y:0), size:CGSize(width:eachWidth, height:height));
+        pointImageView.frame = CGRect(origin:CGPoint(x:eachWidth * 2, y:0), size:CGSize(width:eachWidth, height:height));
+        minuteImageView1.frame = CGRect(origin:CGPoint(x:eachWidth * 3, y:0), size:CGSize(width:eachWidth, height:height));
+        minuteImageView2.frame = CGRect(origin:CGPoint(x:eachWidth * 4, y:0), size:CGSize(width:eachWidth, height:height));
         
     }
     func doTimer(){
-        let timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "timerFireMethod:", userInfo: nil, repeats:true);
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: Selector(("timerFireMethod:")), userInfo: nil, repeats:true);
         timer.fire()
     }
-    func timerFireMethod(timer: NSTimer) {
+    func timerFireMethod(timer: Timer) {
         let now = NSDate();
         
-        let cal = NSCalendar(calendarIdentifier: NSChineseCalendar);
+        let cal = NSCalendar(calendarIdentifier: NSCalendar.Identifier(rawValue: NSChineseCalendar));
         
-        let datecomp1:NSDateComponents = cal!.components(NSCalendarUnit.Hour, fromDate: now);
-        let datecomp2:NSDateComponents = cal!.components(NSCalendarUnit.Minute, fromDate: now);
+        let datecomp1:NSDateComponents = cal!.components(NSCalendar.Unit.hour, from: now as Date) as NSDateComponents;
+        let datecomp2:NSDateComponents = cal!.components(NSCalendar.Unit.minute, from: now as Date) as NSDateComponents;
         let hour = datecomp1.hour;
         let minute = datecomp2.minute;
         let a:Int = hour/10;
@@ -94,7 +94,7 @@ class FirstViewController: UIViewController {
         minuteImageView1.image = UIImage(named: String(format: "%@_number_0%d.png", theme,c));
         minuteImageView2.image = UIImage(named: String(format: "%@_number_0%d.png", theme,d));
         
-        pointImageView.hidden = !pointImageView.hidden;
+        pointImageView.isHidden = !pointImageView.isHidden;
 
     }
     func setTabBarVisible(visible:Bool, animated:Bool) {
@@ -102,21 +102,21 @@ class FirstViewController: UIViewController {
         let frame = self.tabBarController?.tabBar.frame;
         let offsetY : CGFloat = (visible ? -49.0 : 49.0);
         
-        let duration:NSTimeInterval = (animated ? 0.3 : 0.0);
+        let duration:TimeInterval = (animated ? 0.3 : 0.0);
         if frame != nil {
-            UIView.animateWithDuration(duration) {
-                self.tabBarController?.tabBar.frame = CGRectOffset(frame!, 0, offsetY)
+            UIView.animate(withDuration: duration) {
+                self.tabBarController?.tabBar.frame = frame!.offsetBy(dx: 0, dy: offsetY)
                 return
             }
         }
     }
     func tabBarIsVisible() -> Bool{
-        let y1 = self.tabBarController?.tabBar.frame.origin.y;
-        let y2 = CGRectGetMaxY(self.view.frame);
+        let y1: CGFloat = (self.tabBarController?.tabBar.frame.origin.y)!;
+        let y2: CGFloat = self.view.frame.maxY;
         return y1 < y2;
     }
     func tappedView(){
-        setTabBarVisible(!tabBarIsVisible(), animated: true)
+        setTabBarVisible(visible: !tabBarIsVisible(), animated: true)
     }
     
     func swipeViewDown(sender:UISwipeGestureRecognizer){
